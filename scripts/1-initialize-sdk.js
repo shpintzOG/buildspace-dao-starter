7044a645ff3c56fc31d5181eafde06cd6a27255b
coolistkovbel@icloud.com <coolistkovbel@icloud.com>
2021-12-19 22:32:59 -0500

    setup + env

---
+++ scripts/1-initialize-sdk.js
@@ -0,0 +1,37 @@
+
+
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import ethers from 'ethers';
+
+// import and configure .env file that we use to secure store out env variable
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Quick checks
+if(!process.env.PRIVATE_KEY || process.env.PRIVATE_KEY == ""){
+  console.log("🤦🏽‍♂️ private key not found")
+}
+
+
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    // Your wallet private key.
+    process.env.PRIVATE_KEY,
+    // RPC URL
+    ethers.getDefaultProvider(process.env.ALCHEMY_API_URL),
+  ),
+);
+
+(async () => {
+  try {
+    const apps = await sdk.getApps();
+    console.log(`Your app address is: ${apps[0].address}`);
+  }catch (err) {
+    console.error(`failed  to get apps from sdk ${err}`);
+    process.exit(1);
+  }
+})()
+
+// export the intialized thirdweb SDK to use in other scripts
+export default sdk;
